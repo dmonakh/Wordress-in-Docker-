01363d06d8658d13e20701cf28291cdfaae802fe
dmonakh <d.monakh@it-sprout.org>
2023-04-05 18:28:57 +0300

    Create docker-compose and wp config

---
+++ wp-config.php
@@ -0,0 +1,6 @@
+define('DB_NAME', 'MyWordPressTest');
+define('DB_USER', 'MyWordPressTest');
+define('DB_PASSWORD', 'wordsec');
+define('DB_HOST', 'db');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
