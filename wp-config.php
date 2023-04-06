7ce230184910f3d27087ef019dd2d1a5f43d4ac7
dmonakh <d.monakh@it-sprout.org>
2023-04-06 15:04:29 +0300

    edit docker-compose

--- wp-config.php
+++
@@ -1,6 +0,0 @@
-define('DB_NAME', 'MyWordPressTest');
-define('DB_USER', 'MyWordPressTest');
-define('DB_PASSWORD', 'wordsec');
-define('DB_HOST', 'db');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
